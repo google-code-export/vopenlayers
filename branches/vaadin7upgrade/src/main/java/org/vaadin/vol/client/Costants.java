57897123bdbc92fc42c8bd0c58277f9467f80520
giancarlo.domanico@gmail.com <giancarlo.domanico@gmail.com@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2013-02-20 20:12:28 +0000

    added more costansts - just a test of commit

--- branches/vaadin7upgrade/src/main/java/org/vaadin/vol/client/Costants.java
+++ branches/vaadin7upgrade/src/main/java/org/vaadin/vol/client/Costants.java
@@ -1,14 +1,38 @@
 package org.vaadin.vol.client;
 
-public class Costants {
-	public static final String STYLEMAP_UNIQUEVALUERULES = "olStyleMapUVR";
-	public static final String STYLEMAP_UNIQUEVALUERULES_KEYS = "olStyleMapUVR_uvrkeys";
-	public static final String STYLEMAP_UNIQUEVALUERULES_PREFIX ="olStyleMapUVR_";
-	public static final String STYLEMAP_UNIQUEVALUERULES_INTENT_SUFFIX ="_intent";
-	public static final String STYLEMAP_UNIQUEVALUERULES_PROPERTY_SUFFIX ="_property";
-	public static final String STYLEMAP_UNIQUEVALUERULES_CONTEXT_SUFFIX ="_context";
-	public static final String STYLEMAP_UNIQUEVALUERULES_LOOKUPKEYS_SUFFIX ="_lookupkeys";
-	public static final String STYLEMAP_UNIQUEVALUERULES_LOOKUPITEM_SUFFIX ="_lookup_";
-	
-	
+public class Constants {
+    public static final String STYLEMAP_UNIQUEVALUERULES = "olStyleMapUVR";
+    public static final String STYLEMAP_UNIQUEVALUERULES_KEYS = "olStyleMapUVR_uvrkeys";
+    public static final String STYLEMAP_UNIQUEVALUERULES_PREFIX = "olStyleMapUVR_";
+    public static final String STYLEMAP_UNIQUEVALUERULES_INTENT_SUFFIX = "_intent";
+    public static final String STYLEMAP_UNIQUEVALUERULES_PROPERTY_SUFFIX = "_property";
+    public static final String STYLEMAP_UNIQUEVALUERULES_CONTEXT_SUFFIX = "_context";
+    public static final String STYLEMAP_UNIQUEVALUERULES_LOOKUPKEYS_SUFFIX = "_lookupkeys";
+    public static final String STYLEMAP_UNIQUEVALUERULES_LOOKUPITEM_SUFFIX = "_lookup_";
+
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy/MM/dd";
+    public static final String DEFAULT_DATETIME_FORMAT = DEFAULT_DATE_FORMAT
+            + " " + DEFAULT_TIME_FORMAT;
+
+    public static final String VOL_INHERIT = "__VOL_INHERIT";
+    public static final String VOL_CONTEXT = "__VOL_CONTEXT";
+
+    public static final String VOL_EXTERNALGRAPHIC = "__VOL_EXTERNALGRAPHIC";
+    public static final String VOL_GRAPHICWIDTH = "__VOL_GRAPHICWIDTH";
+    public static final String VOL_GRAPHICHEIGHT = "__VOL_GRAPHICHEIGHT";
+
+    public static final String VOL_ANIMATED_STYLES = "vol_animated_styles";
+    public static final String VOL_ANIMATED_URLICONS = "vol_animated_urlicons";
+    public static final String VOL_ANIMATED_TIMESTAMPS = "vol_animated_timestamps";
+    public static final String VOL_ANIMATED_TYPE = "vol_animated_type";
+    public static final String VOL_ANIMATED_DURATION = "vol_animated_duration";
+    public static final String VOL_ANIMATED_SPEED = "vol_animated_speed";
+    public static final String VOL_ANIMATED_DELAY = "vol_animated_delay";
+    public static final String VOL_ANIMATED_TAIL = "vol_animated_tail";
+    public static final String VOL_ANIMATED_RENDERINTENTS = "vol_animated_renderintent";
+    public static final String VOL_ANIMATED_ATTRIBUTES = "vol_animated_attributes";
+    public static final String VOL_ANIMATED_PLAY = "vol_animated_play";
+    
+    public static final String VOL_POLYGON_INNERRINGS =  "innerrings";
 }
